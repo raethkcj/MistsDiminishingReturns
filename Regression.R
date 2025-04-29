004cf398c690f8904524366fa8d56a22d913865f
Casey Raethke <casey.j.raethke@gmail.com>
2025-04-28 20:11:59 -0500

    Add error handling on failed fit

--- Regression.R
+++ Regression.R
@@ -14,20 +14,28 @@ for (i in seq_len(nrow(classes))) {
 
   stat_data <- read.csv(paste0(class, ".csv"), header = TRUE)
 
-  dodge_fit <- nls(
-    dodge ~ 1 / (1 / C_d + k / (dodgeFromRatingPreDR + dodge_per_agi * agility))
-      + base_dodge + base_agility * dodge_per_agi,
-    data = stat_data,
-    start = list(k = 0.9, C_d = 75)
-  )
-  print(summary(dodge_fit))
+  tryCatch ({
+    dodge_fit <- nls(
+      dodge ~ 1 / (1 / C_d + k / (dodgeFromRatingPreDR + dodge_per_agi * agility))
+        + base_dodge + base_agility * dodge_per_agi,
+      data = stat_data,
+      start = list(k = 0.9, C_d = 75)
+    )
+    print(summary(dodge_fit))
+  }, error = function(e) {
+    print(paste0("Failed to fit dodge for ", class, ": ", e))
+  })
 
   if (base_parry > 0) {
-    parry_fit <- nls(
-      parry ~ 1 / (1 / C_p + k / (parryFromRatingPreDR + parry_per_strength * strength))
-        + base_parry + base_strength * parry_per_strength,
-      data = stat_data,
-      start = list(k = 0.9, C_p = 200))
-    print(summary(parry_fit))
+    tryCatch ({
+      parry_fit <- nls(
+        parry ~ 1 / (1 / C_p + k / (parryFromRatingPreDR + parry_per_strength * strength))
+          + base_parry + base_strength * parry_per_strength,
+        data = stat_data,
+        start = list(k = 0.9, C_p = 200))
+      print(summary(parry_fit))
+    }, error = function(e) {
+      print(paste0("Failed to fit parry for ", class, ": ", e))
+    })
   }
 }
