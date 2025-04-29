ffc2b51293c9e707cf7598771737e6dce89013bf
Casey Raethke <casey.j.raethke@gmail.com>
2025-04-28 20:20:29 -0500

    Add fixed k-values

--- Regression.R
+++ Regression.R
@@ -11,6 +11,7 @@ for (i in seq_len(nrow(classes))) {
   base_dodge <- classes[i, "base_dodge"]
   parry_per_strength <- classes[i, "parry_per_strength"]
   dodge_per_agi <- classes[i, "dodge_per_agi"]
+  k <- classes[i, "k"]
 
   stat_data <- read.csv(paste0(class, ".csv"), header = TRUE)
 
@@ -19,7 +20,7 @@ for (i in seq_len(nrow(classes))) {
       dodge ~ 1 / (1 / C_d + k / (dodgeFromRatingPreDR + dodge_per_agi * agility))
         + base_dodge + base_agility * dodge_per_agi,
       data = stat_data,
-      start = list(k = 0.9, C_d = 75)
+      start = list(C_d = 75)
     )
     print(summary(dodge_fit))
   }, error = function(e) {
@@ -32,7 +33,7 @@ for (i in seq_len(nrow(classes))) {
         parry ~ 1 / (1 / C_p + k / (parryFromRatingPreDR + parry_per_strength * strength))
           + base_parry + base_strength * parry_per_strength,
         data = stat_data,
-        start = list(k = 0.9, C_p = 200))
+        start = list(C_p = 200))
       print(summary(parry_fit))
     }, error = function(e) {
       print(paste0("Failed to fit parry for ", class, ": ", e))
